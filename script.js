6efbd0c6f02cfdcd5c1ac51221c1106d601cf5cd
eklavyar793-ui <eklavyar793@gmail.com>
2026-07-18 06:58:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,64 @@
+alert("script.js loaded");
+console.log("script.js loaded");
+// 1. LOGIN FUNCTION
 function loginUser(email, password) {
     if (!email || !password) {
         alert("Please enter both email and password!");
         return;
     }
+
     console.log("Attempting login for:", email);
-    alert("Button is working perfectly!\nEmail: " + email + "\nPassword: " + password);
+
+    //// Firebase Auth logic for login
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+        alert("Login successful!");
+        window.location.href = "dashboard.html"; // Successful login goes to dashboard
+    })
+    .catch((error) => {
+        alert("Login Error: " + error.message);
+    });
+}
+
+// 2. SIGNUP FUNCTION
+function registerUser(name, email, password) {
+    if (!name || !email || !password) {
+        alert("Please fill in all fields (Name, Email, and Password)!");
+        return;
+    }
+
+    console.log("Attempting registration for:", name);
+
+    // // Firebase Auth logic for signup
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+        alert("Account created successfully!");
+        window.location.href = "index.html"; // Account created, go back to login
+    })
+    .catch((error) => {
+        alert("Signup Error: " + error.message);
+    });
+}
+function logoutUser() {
+    firebase.auth().signOut()
+    .then(() => {
+        window.location.href = "index.html";
+    })
+    .catch((error) => {
+        alert(error.message);
+    });
+}
+function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+    .then((result) => {
+        alert("Google Login Successful!");
+        window.location.href = "dashboard.html";
+    })
+    .catch((error) => {
+        alert("Google Login Error: " + error.message);
+    });
 }
